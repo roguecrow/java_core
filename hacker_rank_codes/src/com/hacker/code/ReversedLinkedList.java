2a270302c7c6f29dce4a192106441e5e9b01fb97
roguecrow <baburajkumar2001@gmail.com>
2024-04-29 17:07:18 +0530

    used LinkedList form (Collections)

---
+++ hacker_rank_codes/src/com/hacker/code/ReversedLinkedList.java
@@ -0,0 +1,70 @@
+package com.hacker.code;
+
+import java.util.Scanner;
+
+
+ class Node {  
+    int data;  
+    Node next;  
+    public Node(int d) {  
+        data = d;  
+    }  
+}  
+
+public class ReversedLinkedList {
+	
+	static void displayList(Node head) {
+		while(head!= null) {
+			System.out.print(head.data+" ");
+			head = head.next;
+		}  
+	}
+	
+	static void reverseEvenNo(Node head) {
+		
+		Node previous = null;
+		Node current = head;
+		Node next;
+		
+		while(current != null) {
+			if(current.data % 2 == 0) {
+				next = current.next;
+				current.next = previous;
+				previous = current;
+				current = next;
+			}
+			else {
+				displayList(previous);
+				previous=null;
+				System.out.print(current.data+" ");
+
+				current = current.next;
+			}
+		}
+		displayList(previous);
+		
+	}
+	
+ 
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size :");
+		int size = sc.nextInt();
+		
+		System.out.println("Enter the list :");
+		Node head = new Node(sc.nextInt());
+		Node current = head;
+		
+		for(int i = 0;i<size -1 ;i++) {
+			current.next = new Node(sc.nextInt());
+			current = current.next;
+		}
+		//displayList(head);
+		reverseEvenNo(head);
+
+	}
+
+}
+
+
+
